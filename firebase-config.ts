2953eeb39352c0bdc40be1e1069d4efc6233b5df
ishankaggarwal <ishank193aggarwal@gmail.com>
2024-01-07 01:41:16 -0800

    Setup firebase

---
+++ firebase-config.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4APg25wXO_ea9IAdrfqueQqld2wC4gBs",
+  authDomain: "nextjs-email-b58b8.firebaseapp.com",
+  projectId: "nextjs-email-b58b8",
+  storageBucket: "nextjs-email-b58b8.appspot.com",
+  messagingSenderId: "121348569373",
+  appId: "1:121348569373:web:e6f47f1a58c0a33a2749f4"
+};
+
+const app = initializeApp(firebaseConfig);
